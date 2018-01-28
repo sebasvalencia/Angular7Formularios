ac7a4766e6739cbe92c17c3f7c9e33290e0d26f9
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-01-28 18:30:49 -0500

    Septima app en Angular - Formularios - creando validaciones para los campos del formulario

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -5,15 +5,19 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
 
 
   usuario: Object = {
-    nombre: "Sebastian",
-    apellido: "Valencia",
-    email: ""
+    nombre: null,
+    apellido: null,
+    email: null
   }
 
   constructor() { }
