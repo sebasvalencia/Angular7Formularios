d5c96e26d34e94a7d51ea041127773213835cc17
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-01-31 19:13:24 -0500

    Septima app en Angular - Formularios - se enlaza inputs a propiedades del formGroup y vcalidaciones

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -17,7 +17,7 @@ export class DataComponent implements OnInit {
 
     this.forma = new FormGroup({
       //'nombre':new FormControl('Sebas'),
-      'nombre': new FormControl('', Validators.required),
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
       'apellido': new FormControl('', Validators.required),
       'correo': new FormControl('', [Validators.required, 
                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
