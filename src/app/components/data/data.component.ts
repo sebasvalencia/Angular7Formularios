1bc10f1f57b58d0a26038567697c84384dae71ef
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-01 19:03:00 -0500

    Septima app en Angular - Formularios - se setean datos y se resetean datos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -12,8 +12,8 @@ export class DataComponent implements OnInit {
 
   forma: FormGroup;
 
-  usuario: Object = {
-    nombreCompleto: {
+  usuario: any = {
+    nombrecompleto: {
       nombre: "Sebastian",
       apellido: 'Valencia'
     },
@@ -29,14 +29,17 @@ export class DataComponent implements OnInit {
 
       'nombrecompleto': new FormGroup({
         //'nombre':new FormControl('Sebas'),
-        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-        'apellido': new FormControl('', Validators.required)
+        //this.usuario.nombrecompleto.nombre -> precargar informacion
+        'nombre': new FormControl(this.usuario.nombrecompleto.nombre, [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl(this.usuario.nombrecompleto.apellido, Validators.required)
       }),
 
-      'correo': new FormControl('', [Validators.required,
+      'correo': new FormControl(this.usuario.correo, [Validators.required,
       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
       ])
     });
+
+    this.forma.setValue(this.usuario);
 
 
   }
@@ -44,6 +47,20 @@ export class DataComponent implements OnInit {
   guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma);
+    //this.forma.reset( this.usuario );
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    });
+
+    //Otra forma, no es la mejor
+    //this.forma.controls['correo'].setValue('nuevocorreo@setvalue.com');
+
+
+
   }
 
   ngOnInit() {
